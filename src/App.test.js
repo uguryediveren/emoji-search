4e2fc2545d23870ed9a2b61c0b478beb2db9b883
Hamdi Uğur Yediveren <ugur_yed@hotmail.com>
2022-08-03 16:55:59 +0300

    test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,45 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import '@testing-library/jest-dom';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from "./App";
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
+
+describe("App", () => {
+  test("renders header without crashing", () => {
+    render(<App />);
+    expect(screen.getByText("Emoji Search")).toBeInTheDocument();
+  });
+
+  test("renders all emoji without crashing", () => {
+    render(<App />);
+    expect(screen.getAllByTestId("emoji-result-row")).toHaveLength(20);
+  });
+
+  test("renders search input without crashing", () => {
+    render(<App />);
+    let searchText = "100";
+    const searchInput = screen.getAllByPlaceholderText("Search");
+    userEvent.type(searchInput, searchText);
+    expect(screen.getByText(searchText)).toBeInTheDocument();
+  });
+
+  test("Emoji kopyalanır", () => {
+    const item = screen.getByText("Click to copy emoji");
+    item.click();
+    expect(item).toHaveAttribute("data-clipboard-text", "Click to copy emoji");
+  });
+
+  // TODO //
+  // test("copy to clipboard when clicked", async () => {
+  //   render(<App />);
+  //   const user = userEvent.setup();
+  //   let clickableText = await screen.getAllByTestId("emoji-result-row")[0].textContent;
+  //   console.log("clickableText", clickableText);
+  //   userEvent.click(clickableText);
+  //   const contents = await navigator.clipboard.readText();
+  //   console.log("contents", contents);
+  // });
+  // TODO //
+
 });
--- src/SearchInput.js
+++ src/SearchInput.js
@@ -16,7 +16,7 @@ export default class SearchInput extends PureComponent {
     return (
       <div className="component-search-input">
         <div>
-          <input onChange={this.handleChange} />
+          <input placeholder='Search' onChange={this.handleChange} />
         </div>
       </div>
     );
